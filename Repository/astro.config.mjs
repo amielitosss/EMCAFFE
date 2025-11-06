1d610af7809cc928f1e7e51a2ad99d746268d8fe
Balita <carlbalita@mode83.onmicrosoft.com>
2025-11-06 12:08:08 +0100

    astrto update

--- Repository/astro.config.mjs
+++ Repository/astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://<your-username>.github.io/EMCAFFE', 
+  site: 'https://amielitosss.github.io/EMCAFFE', 
   base: '/EMCAFFE/',                                 
   integrations: [tailwind()],
   redirects: {
