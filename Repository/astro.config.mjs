d65c6dc1352914a025486a23f3f6154aba4a9223
Balita <carlbalita@mode83.onmicrosoft.com>
2025-11-28 12:19:58 +0100

    Update Astro + Netlify adapter

--- Repository/astro.config.mjs
+++ Repository/astro.config.mjs
@@ -1,20 +1,12 @@
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  output: 'server',
+  adapter: netlify(),
   integrations: [tailwind()],
-
   redirects: {
     '/': '/fr',
-  },
-
-  output: 'server',
-
-  vite: {
-    resolve: {
-      alias: {
-        '@': new URL('./src', import.meta.url).pathname,
-      },
-    },
   },
 });
