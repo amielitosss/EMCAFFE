7d2ee69bf9b630a35185ae6332194d2c687644e4
konchu974 <dugain.c@outlook.com>
2025-12-08 13:26:32 +0100

    render

--- Repository/astro.config.mjs
+++ Repository/astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
+import node from '@astrojs/node'; // ← Changez l'adapter
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   output: 'server',
-  adapter: netlify({
-    edgeMiddleware: false,
-    functionPerRoute: false  // Ajoutez ceci
+  adapter: node({
+    mode: 'standalone' // ← Important pour Render
   }),
   integrations: [tailwind()],
-  site: 'https://emcafe.netlify.app', // Ajoutez votre URL
 });
